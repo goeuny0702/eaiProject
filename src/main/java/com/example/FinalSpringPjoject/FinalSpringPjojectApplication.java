79c9c95d06a3c1c9983134fb324f02f9fa9fe69f
[goeun] <[zz4442@naver.com]>
2025-06-02 10:36:05 +0900

    Initial commit

---
+++ src/main/java/com/example/FinalSpringPjoject/FinalSpringPjojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinalSpringPjoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSpringPjojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalSpringPjojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FinalSpringPjoject/FinalSpringPjojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinalSpringPjoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSpringPjojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
