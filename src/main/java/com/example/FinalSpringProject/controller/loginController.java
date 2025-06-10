2c898f1e8421039ccec0f98257637b5e304792c7
ParkJaeHyun <mrone1126@gmail.com>
2025-06-10 09:51:16 +0900

    feat: login page

--- src/main/java/com/example/FinalSpringProject/controller/loginController.java
+++ src/main/java/com/example/FinalSpringProject/controller/loginController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class signinController {
-
-    @GetMapping("/signin")
-    public String showSignInPage() {
-        return "signin";
+public class loginController {
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
     }
 }
