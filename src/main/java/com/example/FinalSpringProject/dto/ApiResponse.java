07fe0979775666c700568e2b7fdac151f4e1ff2e
ParkJaeHyun <mrone1126@gmail.com>
2025-06-13 10:09:14 +0900

    feat: forn entity service define

--- src/main/java/com/example/FinalSpringProject/dto/ApiResponse.java
+++
@@ -1,9 +0,0 @@
-//package com.example.FinalSpringProject.dto;
-//
-//import lombok.Data;
-//import java.util.List;
-//
-//@Data
-//public class ApiResponse {
-//    private List<TrainingItem> data;
-//}
